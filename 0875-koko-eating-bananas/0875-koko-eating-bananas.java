42be342666401158695bc4c92265b336cd36bf4c
rajanayakdevops <rajanayak.devops@gmail.com>
2026-05-16 11:24:52 +0530

    Time: 7 ms (79%), Space: 47.9 MB (55.9%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public boolean check(int[] arr, int mid , int h ){
+        long hours = 0;
+
+
+
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]<= mid){
+                hours++;
+            }else{
+                hours += (arr[i]+mid-1)/mid;
+            }
+
+            // System.out.println(mid +" "+ (i/nt)Math.ceil(arr[i]/mid) + " "+ hours);
+        }
+
+        return hours <= h;
+
+        
+
+    }
+    public int minEatingSpeed(int[] arr, int h) {
+        
+        int min = 1;
+        int max = Integer.MIN_VALUE;
+
+        for(int val : arr){
+            //  min = Math.min(min,val);
+             max = Math.max(max,val);
+        }
+
+        int start = min;
+        int end = max;
+
+        int ans = -1;
+
+        while(start <= end ){
+            int mid = start + (end - start)/2;
+
+
+            if(check(arr,mid,h)){
+                end = mid-1;
+                ans = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+
+
+        return ans;
+
+    }
+}
