c471d20a0045237b9d7b2159f3a4160af83ab976
rajanayakdevops <rajanayak.devops@gmail.com>
2026-05-17 22:59:22 +0530

    Time: 0 ms (100%), Space: 116.1 MB (79.74%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findPeakGrid(int[][] mat) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Find max row in mid column
+            int maxRow = 0;
+
+            for (int i = 0; i < m; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
+                }
+            }
+
+            int left = (mid - 1 >= 0) ? mat[maxRow][mid - 1] : -1;
+            int right = (mid + 1 < n) ? mat[maxRow][mid + 1] : -1;
+
+            // Peak found
+            if (mat[maxRow][mid] > left &&
+                mat[maxRow][mid] > right) {
+
+                return new int[]{maxRow, mid};
+            }
+
+            // Move left
+            else if (left > mat[maxRow][mid]) {
+                high = mid - 1;
+            }
+
+            // Move right
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
