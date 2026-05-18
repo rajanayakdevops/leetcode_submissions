9c2d2216dccb42f119895ef37ed58c06b8026ca3
rajanayakdevops <rajanayak.devops@gmail.com>
2026-05-18 14:10:05 +0530

    Time: 179 ms (96.67%), Space: 58.1 MB (73.33%) - LeetHub

--- 3449-maximize-the-minimum-game-score/3449-maximize-the-minimum-game-score.java
+++ 3449-maximize-the-minimum-game-score/3449-maximize-the-minimum-game-score.java
@@ -1,63 +1,63 @@
-class Solution {
+    class Solution {
 
-    private boolean possible(long target, int[] points, int M) {
+        private boolean possible(long target, int[] points, int M) {
 
-        long moves = 0;
-        long extra = 0;
-        long bonus = 0;
+            long moves = 0;
+            long extra = 0;
+            long bonus = 0;
 
-        for (int p : points) {
+            for (int p : points) {
 
-            long required = (target + p - 1L) / p;
+                long required = (target + p - 1L) / p;
 
-            if (extra >= required) {
+                if (extra >= required) {
 
-                extra = 0;
-                bonus++;
+                    extra = 0;
+                    bonus++;
 
-            } else {
+                } else {
 
-                long need = required - extra;
+                    long need = required - extra;
 
-                moves += 2 * need - 1 + bonus;
+                    moves += 2 * need - 1 + bonus;
 
-                extra = need - 1;
-                bonus = 0;
+                    extra = need - 1;
+                    bonus = 0;
+                }
+
+                if (moves > M) {
+                    return false;
+                }
             }
 
-            if (moves > M) {
-                return false;
-            }
+            return true;
         }
 
-        return true;
+        public long maxScore(int[] points, int M) {
+
+            if (M < points.length) {
+                return 0;
+            }
+
+            long low = 1;
+            long high = (long) 1e18;
+            long ans = 0;
+
+            while (low <= high) {
+
+                long mid = low + (high - low) / 2;
+
+                if (possible(mid, points, M)) {
+
+                    ans = mid;
+                    low = mid + 1;
+
+                } else {
+
+                    high = mid - 1;
+                }
+            }
+
+            return ans;
+        }
     }
-
-    public long maxScore(int[] points, int M) {
-
-        if (M < points.length) {
-            return 0;
-        }
-
-        long low = 1;
-        long high = (long) 1e18;
-        long ans = 0;
-
-        while (low <= high) {
-
-            long mid = low + (high - low) / 2;
-
-            if (possible(mid, points, M)) {
-
-                ans = mid;
-                low = mid + 1;
-
-            } else {
-
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-}
