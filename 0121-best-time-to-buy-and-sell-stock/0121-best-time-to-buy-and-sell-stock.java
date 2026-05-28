cd17e1d0660becf88ea8bb2a96139d7c7a0828b7
rajanayakdevops <rajanayak.devops@gmail.com>
2026-05-28 08:08:43 +0530

    Time: 2 ms (39.82%), Space: 88.7 MB (98.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,18 +2,21 @@ class Solution {
     public int maxProfit(int[] arr) {
         
         int n = arr.length;
-        int[] pse = new int[n];
+        int[] previous_smaller_element = new int[n];
 
-        pse[0] = arr[0];
+        // the profit of selling the stock at index i will be => arr[i] - smallest value from range ( 0, i-1);
+
+
+        previous_smaller_element[0] = arr[0];
 
         for(int i = 1;i<n;i++){
-            pse[i] = Math.min(arr[i],pse[i-1]);
+            previous_smaller_element[i] = Math.min(arr[i],previous_smaller_element[i-1]);
         }
 
         int profit = 0;
 
         for(int i = 1;i<n;i++){
-            profit = Math.max(profit,arr[i]-pse[i-1]);
+            profit = Math.max(profit,arr[i]-previous_smaller_element[i-1]);
         }
 
         return profit;
