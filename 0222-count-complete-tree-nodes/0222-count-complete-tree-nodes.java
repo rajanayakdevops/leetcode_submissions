2973a452c0323a40b08b5ae37e423c36f3749490
rajanayakdevops <rajanayak.devops@gmail.com>
2026-06-17 18:09:14 +0530

    Time: 0 ms (100%), Space: 49.2 MB (92.05%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,62 +1,76 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+
+//     public int findleft(TreeNode node){
+        
+//         int count = 0;
+//         while(node != null ){
+//             count++;
+//             node = node.left;
+//         }
+        
+        
+//         return count;
+//     }
+
+//     public int findright(TreeNode node){
+        
+//         int count = 0;
+//         while(node != null ){
+//             count++;
+//             node = node.right;
+//         }
+        
+//         return count;
+//     }
+
+//     public int find(TreeNode node ){
+//         if(node == null ) return 0;
+
+//         int leftheight = findleft(node.left);
+
+//         int rightheight = findright(node.right);
+
+//         if(leftheight == rightheight ){
+//             return  (1<<leftheight+1 )- 1 ;
+//         } 
+
+//         int leftsubtree = find(node.left);
+//         int rightsubtree = find(node.right);
+
+//         return 1+ leftsubtree+rightsubtree;
+        
+//     }
+
+//     public int countNodes(TreeNode node) {
+//         return find(node);
+//     }
+// }
+
 class Solution {
 
-    public int findleft(TreeNode node){
-        
-        int count = 0;
-        while(node != null ){
-            count++;
-            node = node.left;
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
-        
-        
-        return count;
-    }
 
-    public int findright(TreeNode node){
-        
-        int count = 0;
-        while(node != null ){
-            count++;
-            node = node.right;
-        }
-        
-        return count;
-    }
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
 
-    public int find(TreeNode node ){
-        if(node == null ) return 0;
-
-        int leftheight = findleft(node.left);
-
-        int rightheight = findright(node.right);
-
-        if(leftheight == rightheight ){
-            return  (1<<leftheight+1 )- 1 ;
-        } 
-
-        int leftsubtree = find(node.left);
-        int rightsubtree = find(node.right);
-
-        return 1+ leftsubtree+rightsubtree;
-        
-    }
-
-    public int countNodes(TreeNode node) {
-        return find(node);
+        return 1 + leftCount + rightCount;
     }
 }
