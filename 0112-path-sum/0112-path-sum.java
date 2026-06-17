b32ab8715cae7866bf16a78d9cbdbcc259b01ff6
rajanayakdevops <rajanayak.devops@gmail.com>
2026-06-17 11:30:07 +0530

    Time: 0 ms (100%), Space: 45.1 MB (14.24%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -20,6 +20,8 @@ class Solution {
         if(node == null ) return false;
 
         if(node.left == null && node.right == null ){
+
+            // here u need to remember that once we hit the leaf node we need to first subtract the current node val and then we need to check weather target == 0 or not right.... 
             return target == node.val;
         }
 
@@ -30,7 +32,23 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int target) {
         if(root == null ) return false;
 
-        return fun(root,target);
+        // return fun(root,target);
         
+        int sum = 0;
+        return funn(root,target,sum);
+        
+    }
+
+    public boolean funn(TreeNode node ,int target, int sum){
+
+        if(node == null ) return false;
+
+        if(node.left == null && node.right == null ){
+            return sum+node.val == target;
+        }
+
+        return funn(node.left,target,sum+node.val) || funn(node.right,target,sum+node.val);
+
+
     }
 }
