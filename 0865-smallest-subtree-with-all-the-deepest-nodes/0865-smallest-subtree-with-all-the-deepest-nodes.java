7bea7fe72c54e5c0b35a4e37073faa46f5abd1fd
rajanayakdevops <rajanayak.devops@gmail.com>
2026-06-23 07:59:15 +0530

    Time: 0 ms (100%), Space: 43.5 MB (54.17%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -14,39 +14,62 @@
  * }
  */
 
+//  here the brute force apprach might be finding all the depest node and applying the lca logic right like this lca(lca(lca(n1, n2), n3), n4) ...
+
+
 class pair{
     TreeNode node;
     int depth;
-
-    public pair(TreeNode node , int depth ){
+    
+    pair(TreeNode node, int depth){
         this.node = node;
         this.depth = depth;
     }
-
 }
 
 class Solution {
+    public pair find(TreeNode node){
+        if(node == null ){
+            return new pair(null,0);
+        }
 
-    public pair fun(TreeNode node ){
-        if(node == null ) return new pair(null,0);
+        pair left = find(node.left);  // assuming that this will going to store the ans or the lca from the left sub tree and its depth right.
 
-        pair left = fun(node.left);
+        pair right = find(node.right); // and this is going to store the node from the right sub tree and its depth right.
 
-        pair right = fun(node.right);
+        // now i am current node - i need to check which node i need to return back right .
 
-        if(left.depth > right.depth ) return new pair(left.node,left.depth+1);
+        // if the node from the left has larger compare to right or vise versa
+        //else if both the left and right have the same depth then i need to return the current as the lca right.
 
-        if(right.depth > left.depth ) return new pair(right.node, right.depth+1);
+        //REMEMBER ->  to add +1 while returning  
+        
+        if(left.depth > right.depth ){
+            return new pair(left.node, left.depth + 1);
 
-        return new pair(node,left.depth+1); // this line will strik when both the left and right side have the same depth;
+        }
+        if(right.depth > left.depth ){
+            return new pair(right.node, right.depth + 1);
+
+        }
+
+        return new pair(node, left.depth + 1);
+    
+        
+
     }
-
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
 
-    pair result = fun(root); // this function is going to return us a single pair class in which we will be having the TreeNode and the depth associated to that node right.
+// lets have faith in recursion that this is going to return me the desired output
 
-    return result.node;
+// if i try to compare the patter or to identify the patter then i can say that this is similar to lca but in lcs i need to return the lca of the 2 given nodes right but here i need to return the lca of all the deepest node right.
 
+        pair ans = find(root);
 
+        return ans.node;
+
+        // remember that here in this code we are going to return 2 thing the node and the its depth right and one thing to remember that we are going to find the depth recursively right. 
+
+        
     }
 }
