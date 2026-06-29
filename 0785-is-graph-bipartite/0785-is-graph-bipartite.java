bdf42c5ebca9bdd00e9dd0a4d64ec0fab829a12f
rajanayakdevops <rajanayak.devops@gmail.com>
2026-06-29 08:11:27 +0530

    Time: 0 ms (100%), Space: 47.2 MB (37.72%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,13 +1,13 @@
 class Solution {
 
 
-    public boolean dfs(int node,Boolean[] visited, ArrayList<ArrayList<Integer>> adj, boolean flag){
+    public boolean dfs(int node,Boolean[] visited,int[][] graph , boolean flag){
         
         visited[node] = flag;
 
-        for(int i : adj.get(node)){
+        for(int i : graph[node]){
             if(visited[i] == null ){
-                if(dfs(i,visited,adj,!flag) == false) return false ;
+                if(dfs(i,visited,graph,!flag) == false) return false ;
             }
             else if(visited[i] == flag) return false;
         }
@@ -19,21 +19,21 @@ class Solution {
             int n = graph.length;
             Boolean[] visited = new Boolean[n];
 
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
             
-            for(int i = 0;i<n;i++){
-                adj.add(new ArrayList<>());
-            }
+            // for(int i = 0;i<n;i++){
+            //     adj.add(new ArrayList<>());
+            // }
 
-            for(int i = 0;i<n;i++){
-                for(int v : graph[i]){
-                    adj.get(i).add(v);
-                }
-            }
+            // for(int i = 0;i<n;i++){
+            //     for(int v : graph[i]){
+            //         adj.get(i).add(v);
+            //     }
+            // }
 
             for(int i = 0;i<n;i++){
                 if(visited[i] == null ){
-                    if(dfs(i,visited,adj,true) == false) return false;
+                    if(dfs(i,visited,graph,true) == false) return false;
                 }
             }   
 
