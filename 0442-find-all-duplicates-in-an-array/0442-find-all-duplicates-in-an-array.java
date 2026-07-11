5ce10c7823d36c6f81ad1aa07da3d99475726531
rajanayakdevops <rajanayak.devops@gmail.com>
2026-07-11 08:17:33 +0530

    Time: 6 ms (90.32%), Space: 57.7 MB (72.17%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -2,6 +2,8 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> ans = new ArrayList<>();
 
+        // here in this we are making the value at a particular index a negative right. if we see 4 for the first time then we go to index 4-1 and make it negative right and in future if we again see 4 then we again check weather the index 4-1 is positive or negative. this why we can find out weather the number appers twice or not right.... 
+
         for(int i = 0;i<nums.length;i++){
             int index = Math.abs(nums[i])-1;
 
